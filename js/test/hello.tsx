3021ad5fbfeaaf991e9f190a42959b4fd4485078
cgspine <cgspine@gmail.com>
2021-07-10 00:15:44 +0800

    rollback js

--- js/test/hello.tsx
+++ js/test/hello.tsx
@@ -1,27 +1,55 @@
 
-function Item(item, onItemClick){
+function Item1(item, onItemClick){
+    // const [count, setCount] = useState(0)
     const onClick = useCallback(() => {
         onItemClick(item)
+        // setCount(count+1)
     })
 
-    return <Text onClick={ onClick }>{item.name}, {item.count}</Text>
+    return <Text style={{ color:'#fff' }} onClick={ onClick }>偶数：{item.name}, {item.count}</Text>
+
+}
+
+function Item2(item, onItemClick){
+    // const [count, setCount] = useState(0)
+    const onClick = useCallback(() => {
+        onItemClick(item)
+        // setCount(count+1)
+    })
+
+    return <Text style={{ color:'#fff' }} onClick={ onClick }>奇数：{item.name}, {item.count}</Text>
 }
 
 function HelloWorld(current){
+
     const [data, setData] = useState([])
+
     useEffect(() => {
         let ret = []
         for(let i = 0; i < 1000; i++){
-            ret.push({name: 'item' + i, count: 0})
+            ret.push({
+                name: 'item' + i,
+                index: i,
+                count: 0
+            })
         }
         setData(ret)
     }, [current])
+
     let render = function (i) {
         let item = data[i]
-        return Item(item, (it) => {
-            it.count = it.count + 1
-            setData(data)
-        })
+        if(item.index % 2 == 0){
+            return Item1(item, (it) => {
+                it.count = it.count + 1
+                setData(data)
+            })
+        }else{
+            return Item2(item, (it) => {
+                it.count = it.count + 2
+                setData(data)
+            })
+        }
     }
-    return <RecyclerView count = { data.length } render = { render }/>
+
+    return <RecyclerView count = { data.length } render = { render }>Hello World!</RecyclerView>
 }
